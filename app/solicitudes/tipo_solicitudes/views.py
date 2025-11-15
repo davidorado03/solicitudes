869004c10a1fd036b658d7ce6c5b9fe473b18bfa
Luis Javier Vargas Ortiz <luisitog453@gmail.com>
2025-11-14 21:18:03 -0600

    agregando boton para tipo de solicitud

--- app/solicitudes/tipo_solicitudes/views.py
+++ app/solicitudes/tipo_solicitudes/views.py
@@ -26,24 +26,26 @@ def bienvenida(request):
 def lista_solicitudes(request):
     funciones_avanzadas = FuncionesAvanzadas()
     resultado = funciones_avanzadas.calculo_extremo(2, 2)
+
     context = {
         'tipo_solicitudes': TipoSolicitud.objects.all(),
         'resultado': resultado
     }
     return render(request, 'lista_tipo_solicitudes.html', context)
 
+
 def agregar(request):
     if request.method == 'POST':
         form = FormTipoSolicitud(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('lista_tipo_solicitudes')
+            return redirect('lista_tipo_solicitudes')   
     else:
         form = FormTipoSolicitud()
-    context = {
-        'form': form
-    }
-    return render(request, 'agregar_solicitud.html', context)
+
+    return render(request, 'agregar_solicitud.html', {'form': form})
+
+
 
 def obtener_solicitudes(request):
     solicitudes = Solicitud.objects.all()
